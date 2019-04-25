241cf0c3e431a5ba4e906015f75abef3575333d4
Edenn Touitou <deozza@gmail.com>
2019-04-25 12:00:32 +0200

    hihi

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
